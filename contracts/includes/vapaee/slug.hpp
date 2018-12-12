8e5535b059fcc9c7426870f30c74d04dd37cd335
Viterbo Rodriguez <viterbo.rodriguez@imm.gub.uy>
2018-12-12 20:50:19 -0300

    update

--- contracts/includes/vapaee/slug.hpp
+++ contracts/includes/vapaee/slug.hpp
@@ -6,6 +6,7 @@ using namespace std;
 #define longint unsigned long
 #define maxlong 50
 #define bytes 32
+#define nibble 62 
 #define bits 256
 
 namespace vapaee {
@@ -40,34 +41,15 @@ namespace vapaee {
         
 
         char* write_string( char* begin, char* end ) const {
-            // constexpr uint64_t mask = 0x1Full;
-
-            if( (begin + 64) < begin || (begin + 64) > end ) return begin;
-
-            
-
-            return begin;
-        }
-
-        void init(std::string str) {
-            char* ptr = value;
-            longint* d = (longint*) ptr;
+            const char* ptr = value;
             char v, c;
-            int n = std::min( (int) str.length(), maxlong );
             int i = 0;
             int shift;
-
-            // cout << "to_hexa():  " << to_hexa() << "\n"; 
-            
-            // iteración 0
-            i = 0;
-            c = str[i];
-            v = char_to_value( c );
-            // cout << " c: " << c << " v: " << (int)v << " - 0x" << high_part(v) << low_part(v) << "\n";
-
             int ibit, jbit, offset;
+            if( (begin + maxlong) < begin || (begin + maxlong) > end ) return begin;
 
-            for(; i < n; ++i ) {
+
+            for(; i < maxlong; i++ ) {
                 ibit = i*5;
                 int j = (int)(ibit/8);
                 jbit = j*8;
@@ -90,6 +72,87 @@ namespace vapaee {
                     i: 6 j: 3 ibit: 30 jbit: 24 offset: 6 shift: -3
                     i: 7 j: 4 ibit: 35 jbit: 32 offset: 3 shift: 0
 
+                    // Yxxx x000 - 0000 0000 - 0000 0000 - 0000 0000 - 0000 0000 | 3
+                    // 0000 0Yxx - xx00 0000 - 0000 0000 - 0000 0000 - 0000 0000 | -2 
+                    // 0000 0000 - 00Yx xxx0 - 0000 0000 - 0000 0000 - 0000 0000 | 1
+                    // 0000 0000 - 0000 000Y - xxxx 0000 - 0000 0000 - 0000 0000 | -4
+                    // 0000 0000 - 0000 0000 - 0000 Yxxx - x000 0000 - 0000 0000 | -1
+                    // 0000 0000 - 0000 0000 - 0000 0000 - 0Yxx xx00 - 0000 0000 | 2
+                    // 0000 0000 - 0000 0000 - 0000 0000 - 0000 00Yx - xxx0 0000 | -3
+                    // 0000 0000 - 0000 0000 - 0000 0000 - 0000 0000 - 000Y xxxx | 0
+                    */
+                if (shift >= 0) {
+                    v = (31 << shift);
+                    c = ptr[j] & v;
+                    c >>= shift;
+                    cout << " v: " << (int)v << " - 0x" << high_part(v) << low_part(v)
+                         << " ptr[j]: " << " - 0x" << high_part(ptr[j]) << low_part(ptr[j])
+                         << " c: " << " - 0x" << high_part(c) << low_part(c)
+                         << " " << c << "\n";                    
+                } else {
+                    v = 31 >> (-1 * shift);
+                    c = ptr[j] & v;
+                    // 0000 0Yxx
+                    c <<= -1 * shift;
+                    // 000Y xx00
+                    if (j<bytes) {
+                        v = 31 << (8+shift);
+                        v = ptr[j+1] & v;
+                        // xx00 0000
+                        v >>= (8+shift);
+                        c = c | v;
+                    } else {
+                        cout << "BBBBBB";
+                    }
+                    
+                }
+                begin[i] = value_to_char(c);
+            }
+            return begin+i;
+        }
+
+        void init(std::string str) {
+            char* ptr = value;
+            char v, c;
+            int n = std::min( (int) str.length(), maxlong );
+            int i = 0;
+            int shift;
+
+            // cout << "to_hexa():  " << to_hexa() << "\n"; 
+            
+            // iteración 0
+            i = 0;
+            c = str[i];
+            v = char_to_value( c );
+            // cout << " c: " << c << " v: " << (int)v << " - 0x" << high_part(v) << low_part(v) << "\n";
+
+            int ibit, jbit, offset;
+
+            for(; i < n; ++i ) {
+                ibit = i*5;
+                int j = (int)(ibit/8);
+                jbit = j*8;
+                offset = ibit - jbit;
+                shift = 3 - offset;
+                /*
+                cout
+                    << " i: " << i << " j: " << j
+                    << " ibit: " << ibit
+                    << " jbit: " << jbit
+                    << " offset: " << offset
+                    << " shift: " << shift
+                    << "\n";
+                */
+                    /*
+                    i: 0 j: 0 ibit: 0 jbit: 0 offset: 0 shift: 3
+                    i: 1 j: 0 ibit: 5 jbit: 0 offset: 5 shift: -2
+                    i: 2 j: 1 ibit: 10 jbit: 8 offset: 2 shift: 1
+                    i: 3 j: 1 ibit: 15 jbit: 8 offset: 7 shift: -4
+                    i: 4 j: 2 ibit: 20 jbit: 16 offset: 4 shift: -1
+                    i: 5 j: 3 ibit: 25 jbit: 24 offset: 1 shift: 2
+                    i: 6 j: 3 ibit: 30 jbit: 24 offset: 6 shift: -3
+                    i: 7 j: 4 ibit: 35 jbit: 32 offset: 3 shift: 0
+
                     // Yxxx x000 - 0000 0000 - 0000 0000 - 0000 0000 - 0000 0000
                     // 0000 0Yxx - xx00 0000 - 0000 0000 - 0000 0000 - 0000 0000
                     // 0000 0000 - 00Yx xxx0 - 0000 0000 - 0000 0000 - 0000 0000
@@ -101,44 +164,21 @@ namespace vapaee {
 
                     */
 
-                    if (shift >= 0) {
-                        ptr[j] |= (v << shift);
-                        // cout << "to_hexa():  " << to_hexa() << "\n"; 
-                    } else {
-                        ptr[j] |= (v >> (-1 * shift));
+                if (shift >= 0) {
+                    ptr[j] |= (v << shift);
+                    // cout << "to_hexa():  " << to_hexa() << "\n"; 
+                } else {
+                    ptr[j] |= (v >> (-1 * shift));
+                    // cout << "to_hexa():  " << to_hexa() << "\n";
+                    if (j<bytes) {
+                        ptr[j+1] |= (v << 8+shift);
                         // cout << "to_hexa():  " << to_hexa() << "\n";
-                        if (j<bytes) {
-                            ptr[j+1] |= (v << 8+shift);
-                            // cout << "to_hexa():  " << to_hexa() << "\n";
-                        } else {
-                            cout << "AAAAA";
-                        }
-                        // cout << "0x" << high_part(v >> (-1 * shift)) << low_part(v >> (-1 * shift)) << "\n";
+                    } else {
+                        cout << "AAAAA";
                     }
-
+                    // cout << "0x" << high_part(v >> (-1 * shift)) << low_part(v >> (-1 * shift)) << "\n";
+                }
             }
-            /*
-            for(; i < n && i < sizeof(longint); ++i ) {
-                c = str[i];
-                v = char_to_value( c );
-                *d <<= 5;
-                cout << "  *d <<= 5; " << to_hexa() << " -- c: " << c << "\n"; 
-                *d |= v;
-                cout << "  *d |= v;  " << to_hexa() << " -- v: " << (int) v << "\n"; 
-            }
-            */
-
-            /*
-            int n = std::min( (int) str.length(), maxlong );
-            int i = 0;
-            for(; i < n && i < sizeof(longint); ++i ) {
-                top <<= 5;
-                top |= char_to_value( str[i] );
-            }
-            for(; i < n ; ++i ) {
-                low <<= 5;
-                low |= char_to_value( str[i] );
-            }*/
         }
 
         static char char_to_value( char c ) {
@@ -153,6 +193,14 @@ namespace vapaee {
             // eosio_assert( false, (string("") + "character '" + c + "' is not in allowed character set for slug_symbol").c_str() );
 
             return 0; // control flow will never reach here; just added to suppress warning
+        }       
+
+        static char value_to_char( char v ) {
+            if (v == 0) return '.';
+            if (v < 6) return v + '0';
+            if (v < 32) return v + 'a' - 6;
+            cout << "value '" << v << "' is out of range for slug_symbol \n";
+            return '?';
         }       
 
         char int_to_hexa(int n) const {
@@ -178,7 +226,7 @@ namespace vapaee {
         char* write_hexa_string( char* begin, char* end ) const {
             // constexpr uint64_t mask = 0x1Full;
 
-            if( (begin + 64) < begin || (begin + 64) > end ) return begin;
+            if( (begin + nibble) < begin || (begin + nibble) > end ) return begin;
 
             // auto v = value;
             const char* ptr = value;
@@ -198,7 +246,7 @@ namespace vapaee {
         }        
 
         std::string to_hexa() const {
-            char buffer[64];
+            char buffer[nibble];
             char* end = write_hexa_string( buffer, buffer + sizeof(buffer) );
             *end = '\0';
             return std::string(buffer);
@@ -206,7 +254,7 @@ namespace vapaee {
      
 
         std::string to_string() const {
-            char buffer[64];
+            char buffer[maxlong];
             char* end = write_string( buffer, buffer + sizeof(buffer) );
             *end = '\0';
             return std::string(buffer);
--- contracts/testing/main.cpp
+++ contracts/testing/main.cpp
@@ -6,8 +6,11 @@ using namespace vapaee;
 
 int main ()
 {
-    slug a = "zzzzzzzz";
+    slug a = "abcdefghijklmnopqrstuvwxyz.12345";
+    slug b = "1zzzzzzzz21zzzzzzzz21zzzzzzzz21zzzzzzzz21zzzzzzzz2";
 
+    cout << "a.to_hexa(): " << a.to_hexa() << "\n";
+    cout << "b.to_hexa(): " << b.to_hexa() << "\n";
     /*
     slug b = "z";
     
@@ -24,5 +27,9 @@ int main ()
     cout << "d.to_hexa(): " << d.to_hexa() << "\n";
     cout << "d.to_string(): " << d.to_string() << "\n";
     */
+
+    cout << "a.to_string(): " << a.to_string() << "\n";
+    cout << "b.to_string(): " << b.to_string() << "\n";
+
     return 0;
 }
