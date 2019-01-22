70ef381898c95eb0c06bff9b9f6ff5e61c9ac646
Viterbo Rodriguez <viterbo.rodriguez@imm.gub.uy>
2019-01-22 20:28:16 -0300

    save

--- src/app/services/cnt.service.ts
+++ src/app/services/cnt.service.ts
@@ -2,19 +2,25 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ScatterService } from './scatter.service';
 
+export interface Publisher {
+    namespace: string;
+    account: string;
+}
+
 @Injectable()
 export class CntService {
-
-    public publisher: {slug?:string};
+    
+    public publisher: Publisher;
     public contract:string;
     cardsntokens:string = "cardsntokens";
-
+    
     private setReady: Function;
     public waitReady: Promise<any> = new Promise((resolve) => {
         this.setReady = resolve;
     });
     constructor(private scatter: ScatterService) {
         this.contract = this.cardsntokens;
-        this.publisher = {slug:"guest"}
+        this.publisher = {namespace:"guest", account:this.contract};
     }
+
 }
